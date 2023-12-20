ce792e71c4d390dd956f633884ac12bef164e696
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2023-12-20 11:55:33 +0530

    Create 1807E.cpp

---
+++ Interactive-problems & Binary Search/1807E.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int mod = 1000000007;
+#define pii pair<int, int>
+#define N 100005
+int inf = 1e18;
+#define ff first 
+#define ss second
+struct less_than_key
+{
+    bool operator()(const pii &a, const pii &b)
+    {
+        return (a.first - a.second) > (b.first - b.second);
+    }
+};
+
+int gcd(int a, int b)
+{
+    if (a < b)
+        return gcd(b, a);
+    if (b == 0)
+        return a;
+    return gcd(a % b, b);
+}
+
+int mult(int a, int b) // O(1)
+{
+    return ((a % mod) * (b % mod)) % mod;
+}
+
+int exponent(int a, int b)
+{
+    int ans = 1;
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+        {
+            ans = (ans * a) % mod;
+        }
+        a = (a * a) % mod;
+        b /= 2;
+    }
+    return ans;
+}
+
+int inverse(int a) { return exponent(a, mod - 2); }
+
+int fact[N];
+int invFact[N];
+
+void init()
+{
+    fact[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fact[i] = mult(i, fact[i - 1]);
+    }
+
+    invFact[N - 1] = inverse(fact[N - 1]);
+    for (int i = N - 2; i >= 0; i--)
+    {
+        invFact[i] = mult(invFact[i + 1], i + 1);
+    }
+}
+
+int nCr(int n, int r)
+{
+    return mult(fact[n], mult(invFact[n - r], invFact[r]));
+}
+
+int add(int a, int b)
+{ // O(1)
+    int ans = (a + b);
+    if (ans >= mod)
+        ans -= mod;
+    if (ans < 0)
+        ans += mod;
+    return ans;
+}
+
+// int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+vector<vector<int>> dirs;
+
+void pr(vector<int> &v)
+{
+    cout << "Printing vector---\n";
+    for (auto x : v)
+    {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+
+int n;
+vector<int>v,pref;
+
+void print(int lo, int mid) {
+    if(lo>mid) return;
+    cout<<"? "<<(mid-lo+1)<<" ";
+    for(int i=lo;i<=mid;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+void solve()
+{
+    cin>>n;
+    v.resize(n+1);
+    pref.resize(n+1);
+    for(int i=1;i<=n;i++) {
+        cin>>v[i];
+        pref[i]=pref[i-1]+v[i];
+    }
+
+    int lo = 1, hi = n;
+    int ans = n;
+    while(lo<=hi) {
+        int mid = (lo+hi)/2;
+        print(lo,mid);
+
+        int res;
+        cin>>res;
+
+        int aa = pref[mid]-pref[lo-1];
+        if(res>aa) {
+            // ans lies betn lo .... mid
+            ans = mid;
+            hi = mid-1;
+        } else {
+            lo = mid+1;
+        }
+    }
+
+    cout<<"! "<<ans<<endl;
+
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+
+    // init();
+    while (t--)
+    {
+        solve();
+    }
+    // for(tt=1;tt<=t;tt++){
+    //     cin>>s;
+    //     cout<<rec(0,1,1)<<"\n";
+    // }
+    return 0;
+}
